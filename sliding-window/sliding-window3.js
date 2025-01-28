171bbcdc146813724b21ebce9a91c1d78552741c
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2025-01-28 22:20:35 +0100

    Add sliding window example with findLongestSubstring function and update README.md link

---
+++ sliding-window/sliding-window3.js
@@ -0,0 +1,38 @@
+/*
+Write a function called findLongestSubstring, which accepts a string and returns the length of the longest substring with all distinct characters.
+
+findLongestSubstring('') // 0
+findLongestSubstring('rithmschool') // 7
+findLongestSubstring('thisisawesome') // 6
+findLongestSubstring('thecatinthehat') // 7
+findLongestSubstring('bbbbbb') // 1
+findLongestSubstring('longestsubstring') // 8
+findLongestSubstring('thisishowwedoit') // 6
+Time Complexity - O(n)
+*/
+
+function findLongestSubstring(string) {
+  let i = 0;
+  let j = 1;
+
+  let maxLength = 0;
+  while (i < string.length && j < string.length) {
+    let tempLength = j + 1 - i;
+    let setOfSlice = new Set(string.slice(i, j + 1));
+    if (setOfSlice.size === tempLength) {
+      if (tempLength > maxLength) maxLength = tempLength;
+      j++;
+    } else {
+      i++;
+    }
+  }
+  return maxLength;
+}
+
+console.log(findLongestSubstring("")); // 0)
+console.log(findLongestSubstring("rithmschool")); // 7
+console.log(findLongestSubstring("thisisawesome")); // 6
+console.log(findLongestSubstring("thecatinthehat")); // 7
+console.log(findLongestSubstring("bbbbbb")); // 1
+console.log(findLongestSubstring("longestsubstring")); // 8
+console.log(findLongestSubstring("thisishowwedoit")); // 6
