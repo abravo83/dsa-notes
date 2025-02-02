0c6aab93a530be98703d674c80e6633397cf4b53
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2025-02-02 07:37:38 +0100

    Add alternative sliding window implementations as comments

--- sliding-window/sliding-window1.js
+++ sliding-window/sliding-window1.js
@@ -14,6 +14,7 @@ Time Complexity - O(N)
 
 Space Complexity - O(1)
 */
+
 function maxSubarraySum(arr, lgth) {
   if (lgth > arr.length) return null;
 
@@ -40,3 +41,19 @@ console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4));
 console.log(maxSubarraySum([-3, 4, 0, -2, 6, -1], 2));
 console.log(maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2));
 console.log(maxSubarraySum([2, 3], 3));
+
+// A DIFFERENT APPROACH
+// function maxSubarraySum(arr, num){
+//   if (arr.length < num) return null;
+
+//   let total = 0;
+//   for (let i=0; i<num; i++){
+//      total += arr[i];
+//   }
+//   let currentTotal = total;
+//   for (let i = num; i < arr.length; i++) {
+//      currentTotal += arr[i] - arr[i-num];
+//      total = Math.max(total, currentTotal);
+//   }
+//   return total;
+// }
