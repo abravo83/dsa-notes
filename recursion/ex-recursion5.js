cf604350605bfa32b2df4b2764e2bc83b73c2eeb
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2025-02-12 00:32:38 +0100

    Add alternative Fibonacci sequence implementation with commented solution

--- recursion/ex-recursion5.js
+++ recursion/ex-recursion5.js
@@ -21,6 +21,14 @@ function fib(nthNum) {
   return fibArr[nthNum - 1];
 }
 
+/*
+Given solution:
+function fib(n){
+    if (n <= 2) return 1;
+    return fib(n-1) + fib(n-2);
+}
+*/
+
 console.log(fib(4));
 console.log(fib(10));
 console.log(fib(28));
