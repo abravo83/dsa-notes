7ea2f0ef3da0082258b05aaf756ddcb5032562f9
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2025-02-13 00:33:06 +0100

    Add someRecursive function with recursive callback checking

---
+++ recursion/ex-recursion8.js
@@ -0,0 +1,38 @@
+/*
+someRecursive
+Write a recursive function called someRecursive which accepts an array and a callback.
+The function returns true if a single value in the array returns true when passed to the callback. 
+Otherwise it returns false.
+*/
+
+// SAMPLE INPUT / OUTPUT
+// const isOdd = val => val % 2 !== 0;
+
+// someRecursive([1,2,3,4], isOdd) // true
+// someRecursive([4,6,8,9], isOdd) // true
+// someRecursive([4,6,8], isOdd) // false
+// someRecursive([4,6,8], val => val > 10); // false
+
+const isOdd = (val) => val % 2 !== 0;
+function someRecursive(arr, callback) {
+  if (!arr.length) return false;
+  let result = false;
+
+  function someRecursiveHelper(helperArr) {
+    if (!helperArr.length) return;
+    const callBackResult = callback(helperArr[helperArr.length - 1]);
+    if (callBackResult) {
+      result = true;
+      return;
+    }
+    someRecursiveHelper(helperArr.slice(0, helperArr.length - 1));
+  }
+
+  someRecursiveHelper(arr);
+  return result;
+}
+
+console.log(someRecursive([1, 2, 3, 4], isOdd));
+console.log(someRecursive([4, 6, 8, 9], isOdd));
+console.log(someRecursive([4, 6, 8], isOdd));
+console.log(someRecursive([4, 6, 8], (val) => val > 10));
