f9325e7bbe6b9708c59948109cd7e3ecc822717e
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2025-02-16 22:59:46 +0100

    Add recursive flatten function to convert nested arrays into single-level array

---
+++ recursion/ex-recursion9.js
@@ -0,0 +1,35 @@
+/*
+flatten
+Write a recursive function called flatten which accepts an array
+ of arrays and returns a new array with all values flattened.
+*/
+
+// flatten([1, 2, 3, [4, 5] ]) // [1, 2, 3, 4, 5]
+// flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
+// flatten([[1],[2],[3]]) // [1,2,3]
+// flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3
+
+function flatten(arr1) {
+  let result = [];
+
+  function helperFlatten(helperArray) {
+    if (!helperArray.length) return;
+
+    let firstItem = helperArray[0];
+    if (!Array.isArray(firstItem)) {
+      result.push(firstItem);
+      helperArray = helperArray.slice(1);
+    } else {
+      helperArray = [...firstItem, ...helperArray.slice(1)];
+    }
+    return helperFlatten(helperArray);
+  }
+
+  helperFlatten(arr1);
+  return result;
+}
+
+console.log(flatten([1, 2, 3, [4, 5]]));
+console.log(flatten([1, [2, [3, 4], [[5]]]]));
+console.log(flatten([[1], [2], [3]]));
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]));
